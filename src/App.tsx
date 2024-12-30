f046853ad053446670ad452adb4cca32b191f035
Brendan Bax <brendanbax@gmail.com>
2024-12-30 16:48:47 -0600

    move logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,75 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './hooks/useAuth'
+import { LogInView } from './views/LogInView'
+import { WelcomeView } from './views/WelcomeView'
+import { SettingsView } from './views/SettingsView'
+import { TermListView } from './views/TermListView'
+import { TermView } from './views/TermView'
+import { RequireAuth } from './components/RequireAuth'
 
 export const App = () => {
-  return <h1>Hello, world</h1>
+  return (
+    <StrictMode>
+      <AuthProvider>
+        <BrowserRouter>
+          {/* 
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+
+      TODO: understand this more b/c I don't really get it...
+      <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+      Read more here... https://reactrouter.com/start/library/routing#configuring-routes
+      <Route path="concerts">
+        <Route index element={<ConcertsHome />} />
+        <Route path=":city" element={<City />} />
+        <Route path="trending" element={<Trending />} />
+      </Route>
+      */}
+          <Routes>
+            {/* Public routes */}
+            <Route path='/' element={<App />} />
+            <Route path='/log-in' element={<LogInView />} />
+            {/* Protected routes */}
+            <Route
+              path='/welcome'
+              element={
+                <RequireAuth>
+                  <WelcomeView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path='/settings'
+              element={
+                <RequireAuth>
+                  <SettingsView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path='/terms'
+              element={
+                <RequireAuth>
+                  <TermListView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path='/terms/:id'
+              element={
+                <RequireAuth>
+                  <TermView />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </StrictMode>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,6 @@
 import './index.css'
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { Container, createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { App } from './App'
-import { LogInView } from './views/LogInView'
-import { SettingsView } from './views/SettingsView'
-import { TermView } from './views/TermView'
-import { TermListView } from './views/TermListView'
-import { WelcomeView } from './views/WelcomeView'
 
-createRoot(document.getElementById('root') as Container).render(
-  <StrictMode>
-    <BrowserRouter>
-      {/* 
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-
-      TODO: understand this more b/c I don't really get it...
-      <Route element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-
-      Read more here... https://reactrouter.com/start/library/routing#configuring-routes
-      <Route path="concerts">
-        <Route index element={<ConcertsHome />} />
-        <Route path=":city" element={<City />} />
-        <Route path="trending" element={<Trending />} />
-      </Route>
-      */}
-      <Routes>
-        {/* Public routes */}
-        <Route path='/' element={<App />} />
-        <Route path='/log-in' element={<LogInView />} />
-        {/* Protected routes */}
-        <Route path='/welcome' element={<WelcomeView />} />
-        <Route path='/settings' element={<SettingsView />} />
-        <Route path='/terms' element={<TermListView />} />
-        <Route path='/terms/:id' element={<TermView />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById('root') as Container).render(<App />)
