c0c519643db4d1b88e51417e97522380568abc5f
Brendan Bax <brendanbax@gmail.com>
2024-12-30 15:24:23 -0600

    nav

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import useAuth from '../hooks/useAuth'
 
 export const LogInForm = () => {
   const navigate = useNavigate()
-  const { logIn } = useAuth()
   const { state } = useLocation()
 
   const handleLogin = () => {
     // TODO: put this behind a real API call then resolve onSuccess()
-    logIn()
-      .then(() => {
-        navigate(state?.path || '/welcome')
-      })
-      .catch((e) => {
-        console.log(e)
-      })
+    // logIn()
+    //   .then(() => {
+    //     navigate(state?.path || '/welcome')
+    //   })
+    //   .catch((e) => {
+    //     console.log(e)
+    //   })
   }
 
   return (
