4b73a7235a6c02eb7d1448e564357bf75d51750e
Brendan Bax <brendanbax@gmail.com>
2024-12-29 22:19:04 -0600

    add basic routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React, { StrictMode } from "react"
-import { Container, createRoot } from "react-dom/client"
-import "./index.css"
-import { App } from "./App"
+import React, { StrictMode } from 'react'
+import { Container, createRoot } from 'react-dom/client'
+import './index.css'
+import { App } from './App'
+import { WelcomeView } from './views/WelcomeView'
+import { SettingsView } from './views/SettingsView'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-createRoot(document.getElementById("root") as Container).render(
+createRoot(document.getElementById('root') as Container).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* 
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+
+      TODO: understand this more b/c I don't really get it...
+      <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+      Read more here... https://reactrouter.com/start/library/routing#configuring-routes
+      <Route path="concerts">
+        <Route index element={<ConcertsHome />} />
+        <Route path=":city" element={<City />} />
+        <Route path="trending" element={<Trending />} />
+      </Route>
+      */}
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/welcome' element={<WelcomeView />} />
+        <Route path='/settings' element={<SettingsView />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
