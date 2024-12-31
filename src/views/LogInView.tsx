72aca0a138fb7d7ba835c941e2f94908f8947b17
Brendan Bax <brendanbax@gmail.com>
2024-12-30 21:58:46 -0600

    basic styling - the worst

--- src/views/LogInView.tsx
+++ src/views/LogInView.tsx
@@ -1,24 +1,24 @@
 import React, { useEffect } from 'react'
-import { Layout } from '../components/Layout'
 import { LogInForm } from '../components/LogInForm'
 import useAuth from '../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
 
 export const LogInView = () => {
-  let navigate = useNavigate()
-  let auth = useAuth()
+  const navigate = useNavigate()
+  const { authed } = useAuth()
 
   useEffect(() => {
-    if (auth) {
+    if (authed) {
       navigate('/current')
     }
-    return
   }, [])
 
   return (
-    <Layout>
-      <h1>Log In View</h1>
-      <LogInForm />
-    </Layout>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-gray-200'>
+      <div className='bg-white rounded-lg p-4 shadow-lg'>
+        <h1>Homeschool Hours</h1>
+        <LogInForm />
+      </div>
+    </div>
   )
 }
