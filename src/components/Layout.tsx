93c4771da791373efe065f6f8cc10ea8334e3720
Brendan Bax <brendanbax@gmail.com>
2025-01-01 13:25:21 -0600

    add todo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,7 @@ export const Layout = ({ children }) => {
     <div className='flex flex-col md:flex-row'>
       <Nav className='md:h-dvh sticky top-0' />
       <main className='p-4 overflow-y-auto'>
+        {/* TODO: Remove this h-1200 when real content is rendered */}
         <div className='h-[1200px]'>{children}</div>
       </main>
     </div>
