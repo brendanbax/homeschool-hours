b4bf9fd5da36b6c2dc5d48461ad005866f9aa2f9
Brendan Bax <brendanbax@gmail.com>
2025-01-01 16:19:36 -0600

    make layout full width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export const Layout = ({ children }) => {
   return (
     <div className='flex flex-col md:flex-row'>
       <Nav className='md:h-dvh sticky top-0' />
-      <main className='p-4 overflow-y-auto'>
+      <main className='p-4 overflow-y-auto w-full'>
         {/* TODO: Remove this h-1200 when real content is rendered */}
         <div className='h-[1200px]'>{children}</div>
       </main>
